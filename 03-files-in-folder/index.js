9e6a721c2c1bfefd115cb369d3e4a274cdf91558
Nebesnaya94 <kuroi.alice.13@gmail.com>
2022-11-06 23:05:36 +0300

    fix: edit calculation of file size in task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -16,7 +16,7 @@ const folder = filePath.join(__dirname, "secret-folder");
       }
       const fileName = filePath.basename(res, filePath.extname(res));
       const fileExt = filePath.extname(res).slice(1);
-      const fileSize = stat.size / 1000 + "kb";
+      const fileSize = (stat.size / 1024).toFixed(2) + "kb";
       const str = `${fileName} - ${fileExt} - ${fileSize}`;
       console.log(str);
     }
